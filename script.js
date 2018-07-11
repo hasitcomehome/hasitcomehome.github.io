abcb519906439cc946ebe382238a523881517a2c
Louis Bougeard <louis.bougeard@gmail.com>
2018-07-11 15:36:41 +0100

    add time to KO or match time

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var xhttp = new XMLHttpRequest();
+var ko = new Date("2018-07-11T18:00:00Z");
 xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         var latestScores = JSON.parse(xhttp.responseText)[0];
@@ -7,6 +8,12 @@ xhttp.onreadystatechange = function () {
             var engGoals = latestScores.away_team.goals;
             var croGoals = latestScores.home_team.goals;
             document.getElementById("score").innerHTML = "<img src='eng.png' height='20'/>󠁧󠁢󠁥󠁮󠁧󠁿 ENG " + engGoals + " - " + croGoals + " CRO <img src='cro.png' height='20'/>"
+        }
+        var d = Date.now();
+        if(d < ko) {
+            document.getElementById("ko").innerHTML = "Today 19:00";
+        } else {
+            document.getElementById("ko").innerHTML = latestScores.time;
         }
         setTimeout(function () {
             getLatestScores();
@@ -17,6 +24,8 @@ xhttp.onreadystatechange = function () {
 function getLatestScores() {
     xhttp.open("GET", "https://api.hasitcomehome.com", true);
     xhttp.send();
+
+
 }
 
 getLatestScores();
